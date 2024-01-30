fa2697e0abc61feb7fe78358b552fa324a00c38e
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-31 04:39:37 +0900

    Update cmake 01 (#4)

--- cmake/01_include_file/src/main.cpp
+++ cmake/01_include_file/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-// #include "hello_wold/hello_world.hpp"
-#include <hello_wold/hello_world.hpp>
+#include "hello_wold/hello_world.hpp"
 
 int main(int argc, char const *argv[])
 {
